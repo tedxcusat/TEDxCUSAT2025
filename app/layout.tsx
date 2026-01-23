ba9e729b43a6626163f63147dc8d0d5a445b62eb
shreyanithin <shreyanithinr@gmail.com>
2026-01-23 20:35:21 +0530

    SpeedInsights added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Orbitron } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // Initialize Orbitron
 const orbitron = Orbitron({
@@ -106,6 +107,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
